5f1f00cdedb299ca672c58e787afb101d239a7db
Cristobal Lara <cris_99@hotmail.es>
2025-02-13 12:13:52 -0800

    Feature: Add hostname to differenciate between instances

--- process_math.php
+++ process_math.php
@@ -4,8 +4,10 @@
     $operation = escapeshellarg($_POST['operation']);
     $command = escapeshellcmd("python3 math_operations.py $num1 $num2 $operation");
     $output = shell_exec($command);
+    $hostname = gethostname();
 
     echo "<h1>Midterm Exam - Cristobal Lara</h1>";
+    echo "<h2>This code is running on server -> $hostname</h2>";
     echo "<h2>Python Script Result</h2>";
     echo "<div>$output</div>";
 ?>
